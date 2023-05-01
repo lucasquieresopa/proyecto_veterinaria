5da1532f0a489b9a5c26dbee6e44da94a724d1a4
Luka <lucassalanitro32@gmail.com>
2023-05-01 17:57:15 -0300

    implemento inicio de sesion y creo super usuario usuario

--- proyecto_veterinaria/urls.py
+++ proyecto_veterinaria/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),         #apunta al url de /accounts
     path("", include("pages.urls")),        #apunta el URL existente a la app pages
 ]

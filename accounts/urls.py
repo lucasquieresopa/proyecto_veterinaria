7707bf07f1e646b4be669ea0da4c98ccbebddc77
Luka <lucassalanitro32@gmail.com>
2023-05-03 16:23:49 -0300

    creo pantalla y configuro pantalla para signup

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import SignUpView
+
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),   #/accounts/signup muestra la view de signup (la cual es un template .html linkeado a una clase viewSignup)
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
 
 # Create your views here.
+
+class SignupView(generic.CreateView):
+    """SignedView es subclase de CreateView"""
+
+    form_class = UserCreationForm   
+    success_url = reverse_lazy('home')  #indica que ante un registro exitoso se redirija al template 'home.
+                                        #reverse_lazy() y no reverse() porque para las generic class-based views 
+                                        # las URL no se cargan cuando se importa el archivo, entonces reverse_lazy() 
+                                        # las ejecuta cuando esten disponibles
+    template_name = 'registration/signup.html'

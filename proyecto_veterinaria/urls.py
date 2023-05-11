ec025d78ad0eeda300212625468bdfc3bffab03b
Luka <lucassalanitro32@gmail.com>
2023-05-11 19:34:44 -0300

    corrijo algunos textos de error

--- proyecto_veterinaria/urls.py
+++ proyecto_veterinaria/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 #from django.views.generic.base import TemplateView 
-from accounts.views import user_registration_view, logout_view, login_view, account_modif_view, password_reset_view
+from accounts.views import user_registration_view, logout_view, login_view, account_modif_view, password_reset_view, ChangePasswordView
 #from pages.views import HomePageView, ProfilePageView
 from pages.views import home_view
 from django.contrib.auth import views as auth_views
@@ -42,9 +42,9 @@ urlpatterns = [
 
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
-        name='password_change_done'),
+        name='password_success'),
 
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), 
+    path('password_change/', ChangePasswordView.as_view(template_name='registration/password_change.html'), 
         name='password_change'),
 
     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
@@ -55,4 +55,6 @@ urlpatterns = [
     
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
      name='password_reset_complete'), 
+
+    
 ]

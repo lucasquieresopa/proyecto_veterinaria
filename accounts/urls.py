a38fd3d0f271662fb0668ea6269c1830dc583c04
Luka <lucassalanitro32@gmail.com>
2023-05-13 12:21:39 -0300

    implemento templates de reset de contraseña

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,14 +20,15 @@ urlpatterns = [       #apunta a la app de auth
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_succeed.html'), 
         name='password_change_done'),
 
-
-    path('password_reset/', views.password_reset_view, 
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_mail_form.html'), 
         name='password_reset'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('password_reset/', views.password_reset_view, 
+    #     name='password_reset'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_pass_form.html'), name='password_reset_confirm'),
     
-    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_peticion_done.html'),
         name='password_reset_done'),
 
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_succeed.html'),
         name='password_reset_complete'),
 ]
